caf16ae948c10199f12f315546652be5f0878900
Camila Cocuzza <cami_cocu@hotmail.com>
2024-02-19 02:59:51 -0300

    permisos endpoints

---
+++ pb_migrations/1708320772_updated_Estado.js
@@ -0,0 +1,18 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("rn5oogr7yhg1dog")
+
+  collection.listRule = "@request.auth.id != \"\""
+  collection.viewRule = "@request.auth.id != \"\""
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("rn5oogr7yhg1dog")
+
+  collection.listRule = ""
+  collection.viewRule = ""
+
+  return dao.saveCollection(collection)
+})
